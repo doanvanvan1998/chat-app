e7bba07b473f7c5b58e76be62fc47622caf5bd18
Đoàn Văn Vấn <vandv@MacBook-Pro-cua-oan.local>
2025-03-25 14:26:51 +0700

    add axios

---
+++ src/api/axiosClient.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+    baseURL :"http://localhost:8080",
+    headers:{
+        "Content-Type":"application/json",
+    },
+});
+axiosClient.interceptors.request.use((config) => {
+    return config
+
+});
+
+axiosClient.interceptors.response.use(
+    (response) => response,
+    (error) =>{
+        console.log("API Error:",error);
+        return Promise.reject(error)   
+    }
+);
+export default axiosClient
+
+
